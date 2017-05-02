1b6a4ebd04761ef85c9a9b7bf95699e2b328e725
Harrison Booth <harrisonbooth@ymail.com>
2017-05-02 14:29:36 +0100

    add find, delete, save, and get many methods to player class

--- client/models/player.rb
+++ client/models/player.rb
@@ -1,3 +1,5 @@
+require_relative( '../../db/sql_runner.rb' )
+
 class Player
 
   attr_reader( :name, :team, :favourite_heroes )
@@ -7,6 +9,50 @@ class Player
     @name = player_information['name']
     @team = player_information['team']
     @favourite_heroes = player_information['favourite_heroes']
+  end
+
+  def save()
+    sql = "
+    INSERT INTO players
+    ( name, team, favourites_heroes )
+    VALUES
+    ( '#{@name}', '#{@team}', '#{@favourites_heroes}' )
+    RETURNING *;
+    "
+    @id = SqlRunner.run( sql )[0]['id'].to_i()
+  end
+
+  def self.delete( id )
+    sql = "
+    DELETE FROM players
+    WHERE
+    id = #{id;}
+    "
+    SqlRunner.run( sql )
+  end
+
+  def self.find( id )
+    sql = "
+    SELECT * FROM players
+    WHERE
+    id = #{id}
+    "
+    player = SqlRunner( sql )[0]
+    return Player.new( player )
+  end
+
+  def self.update( details )
+
+  end
+
+  def self.all()
+
+  end
+
+# -- Reuseable methods --
+  def self.get_many( sql )
+    returned_player_data = SqlRunner.run( sql )
+    return returned_player_data.map({ |player| Player.new( player ) })
   end
 
 end
