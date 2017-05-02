aacf9b615842bd3e566339127b8b8485df47f7bf
Harrison Booth <harrisonbooth@ymail.com>
2017-05-02 13:53:05 +0100

    add sql database setup file '

---
+++ db/dota_2.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS favourites;
+DROP TABLE IF EXISTS heroes;
+DROP TABLE IF EXISTS players;
+
+CREATE TABLE players(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  team VARCHAR(255),
+)
+
+CREATE TABLE heroes(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+)
+
+CREATE TABLE favourites(
+  id SERIAL8 PRIMARY KEY,
+  player_id INT8 REFERENCES players( id ) ON DELETE CASCADE,
+  hero_id INT8 REFERENCES heroes( id ) ON DELETE CASCADE
+)
