ad841c36317ecf814e7ae2abd804cfb6427838d4
Harrison Booth <harrisonbooth@ymail.com>
2017-05-02 14:37:41 +0100

    add save method to hero class

--- client/models/hero.rb
+++ client/models/hero.rb
@@ -1,3 +1,5 @@
+require_relative( '../../db/sql_runner.rb' )
+
 class Hero
 
   attr_reader( :name, :description )
@@ -5,6 +7,24 @@ class Hero
   def initialize( hero_information )
     @name = hero_information[ 'name' ]
     @description = hero_information[ 'description' ]
+  end
+
+  def save()
+    sql = "
+    INSERT INTO heroes
+    ( name, description )
+    VALUES
+    ( '#{@name}', '#{@description}' )
+    RETURNING *;
+    "
+
+    @id = SqlRunner.run( sql )[0]['id'].to_i()
+  end
+
+  # -- Reuseable methods --
+  def self.get_many( sql )
+    returned_hero_data = SqlRunner.run( sql )
+    return returned_hero_data.map({ |hero| Hero.new( hero ) })
   end
 
 end
