3c31a7d7e1453d7764f13039c9f6f91f100f115a
Harrison Booth <harrisonbooth@ymail.com>
2017-05-02 11:39:58 +0100

    add array index accessor route

--- src/controllers/application_controller.rb
+++ src/controllers/application_controller.rb
@@ -4,3 +4,8 @@ require( 'sinatra/contrib/all' ) if development? #auto refresh on changes
 get( '/test/:firstname/:surname' ) do
   return "<h1>Welcome, #{params[:firstname]} #{params[:surname]}</h1>"
 end
+
+get( '/array/:index' ) do
+  array_of_names = ['Zsolt', 'Harrison', 'John']
+  return "<h1>Staff member: #{array_of_names[params[:index].to_i()]}</h1>"
+end
