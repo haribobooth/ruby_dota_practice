25b9051a55e034a4c06bfa672cd8e6f58fae5cdf
Harrison Booth <harrisonbooth@ymail.com>
2017-05-03 14:46:20 +0100

    amend refresh script and add more styling, refresh script can now be used hwile outsude db filder

--- db/refresh_db.rb
+++ db/refresh_db.rb
@@ -1,2 +1,4 @@
-system('psql -d dota_2 -f dota_2.sql')
-system('psql -d dota_2 -f seeds.sql')
+__path = File.expand_path(File.dirname(__FILE__))
+
+system("psql -d dota_2 -f #{__path}/dota_2.sql")
+system("psql -d dota_2 -f #{__path}/seeds.sql")
--- db/seeds.sql
+++ db/seeds.sql
@@ -6,6 +6,17 @@ INSERT INTO heroes ( name, description ) VALUES ( 'Pudge', 'Drops some fat stack
 INSERT INTO heroes ( name, description ) VALUES ( 'Natures Prophet', 'Ratting expert' );
 INSERT INTO heroes ( name, description ) VALUES ( 'Meepo', 'Why would you even?' );
 INSERT INTO heroes ( name, description ) VALUES ( 'Naga Siren', 'Whos team does this ult even help?' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Anti-mage', 'Anti-fun*' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Axe', '*get dunked on' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Crystal Maiden', 'The definitive support' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Dazzle', 'The OTHER definitive support' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Drow Ranger', 'Dont even think about it' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Earthshaker', 'Better at dunking than Axe' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Lich', 'Delta split' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Lina', 'ice frog pls nerf' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Lion', 'ice frog pls nerf' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Mirana', 'a worse Luna' );
+INSERT INTO heroes ( name, description ) VALUES ( 'Morphling', 'He is mighty morphing' );
 
 INSERT INTO favourites ( player_id, hero_id ) VALUES ( 1, 1 );
 INSERT INTO favourites ( player_id, hero_id ) VALUES ( 2, 2 );
