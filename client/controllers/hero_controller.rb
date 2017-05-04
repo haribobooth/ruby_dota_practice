19f1e5f14895625f5273636a26688eac285125af
Harrison Booth <harrisonbooth@ymail.com>
2017-05-04 10:18:18 +0100

    comment controllers folder

--- client/controllers/hero_controller.rb
+++ client/controllers/hero_controller.rb
@@ -4,19 +4,23 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/player.rb')
 require_relative( '../models/hero.rb')
 
+# Route to GET list of heroes
 get( '/heroes' ) do
   @heroes = Hero.all()
   erb( :"heroes/index" )
 end
 
+# Route to GET particular hero details
 get( '/heroes/:id/from/:player_id' ) do
   @hero = Hero.find( params[ 'id' ].to_i() )
-  previous_id = params[ 'player_id' ].to_i()
 
+  # Takes in where the user had navigated to here from to make back button
+  previous_id = params[ 'player_id' ].to_i()
   if(previous_id != 0)
     @previous_url = '/players/' + previous_id.to_s()
   else
     @previous_url = '/heroes'
   end
+
   erb( :"heroes/details" )
 end
--- client/controllers/player_controller.rb
+++ client/controllers/player_controller.rb
@@ -4,20 +4,24 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/player.rb')
 require_relative( '../models/hero.rb')
 
+# Route to GET list of players
 get( '/players' ) do
   @players = Player.all()
   erb( :"players/index" )
 end
 
+# Route to GET new player form
 get( '/players/new') do
   erb( :"players/new" )
 end
 
+# Route to GET particular edit player form
 get( '/players/:id/edit' ) do
   @player = Player.find( params[ 'id' ].to_i() )
   erb( :"players/edit" )
 end
 
+# Route to GET particular player details
 get( '/players/:id' ) do
   @player = Player.find( params[ 'id' ].to_i() )
   @heroes = Hero.all()
@@ -25,26 +29,31 @@ get( '/players/:id' ) do
   erb( :"players/details" )
 end
 
+# Route to POST new player to player list
 post('/players') do
   Player.new( params ).save()
   redirect to( '/players' )
 end
 
+# Route to DELETE particular player
 post( '/players/:id/delete' ) do
   Player.delete( params[ 'id' ].to_i() )
   redirect to( '/players' )
 end
 
+# Route to PUT/PATCH (update) particular player
 post( '/players/:id/update' ) do
   Player.update( params )
   redirect to( '/players/' + params[ 'id' ].to_s() )
 end
 
+# Route to POST a new favourite to link a particular player and a particular hero
 post( '/players/:id/favourites' ) do
   Player.add_favourite( params[ 'id' ].to_i(), params[ 'hero_id' ].to_i() )
   redirect to( '/players/' + params[ 'id' ].to_s() )
 end
 
+# Route to DELETE all favourite links between a particular player and a particular hero
 post( '/players/:id/favourites/delete' ) do
   Player.remove_favourite( params[ 'id' ].to_i(), params[ 'hero_id' ].to_i() )
   redirect to( '/players/' + params[ 'id' ].to_s() )
