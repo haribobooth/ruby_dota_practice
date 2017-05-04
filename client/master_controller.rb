8776eb148d8ccd79f73e096914dcb6577c3dde76
Harrison Booth <harrisonbooth@ymail.com>
2017-05-04 10:28:50 +0100

    comment master controller

--- client/master_controller.rb
+++ client/master_controller.rb
@@ -4,6 +4,7 @@ require( 'sinatra/contrib/all' ) if development?
 require_relative( './controllers/player_controller.rb' )
 require_relative( './controllers/hero_controller.rb')
 
+# Route to GET the index page with links to other pages
 get( '/' ) do
   erb(:index)
 end
