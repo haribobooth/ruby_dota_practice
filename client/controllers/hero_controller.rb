aa77ca500f419fc12d336e7e1d9aa0d27c83c164
Harrison Booth <harrisonbooth@ymail.com>
2017-05-04 09:36:09 +0100

    refactor back button logic intro controller

--- client/controllers/hero_controller.rb
+++ client/controllers/hero_controller.rb
@@ -11,6 +11,12 @@ end
 
 get( '/heroes/:id/from/:player_id' ) do
   @hero = Hero.find( params[ 'id' ].to_i() )
-  @previous_id = params[ 'player_id' ].to_i()
+  previous_id = params[ 'player_id' ].to_i()
+
+  if(previous_id != 0)
+    @previous_url = '/players/' + previous_id.to_s()
+  else
+    @previous_url = '/heroes'
+  end
   erb( :"heroes/details" )
 end
